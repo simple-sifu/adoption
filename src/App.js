8f129acaaf7bbe32d1057a604b0e432982bc02b3
Tommy Han <tommy.han@coxautoinc.com>
2019-04-15 16:26:05 -0400

    chg from function to class

--- src/App.js
+++ src/App.js
@@ -5,25 +5,29 @@ const Pet = (props) => {
         React.createElement("h2", {}, props.breed)
     ]);
 }
-const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement(('h1'), {}, 'Adopt Me!'),
-        React.createElement(Pet, {
-            name: "Luna",
-            animal: "dog",
-            breed: "Havanese"
-        }),
-        React.createElement(Pet, {
-            name: "Pepper",
-            animal: "bird",
-            breed: "Cockatiel"
-        }),
-        React.createElement(Pet, {
-            name: "Doink",
-            animal: "cat",
-            breed: "Mixed"
-        })
-    ])
+
+class App extends React.Component {
+    render() {
+        return React.createElement("div", {}, [
+            React.createElement(('h1'), {}, 'Adopt Me!'),
+            React.createElement(Pet, {
+                name: "Luna",
+                animal: "dog",
+                breed: "Havanese"
+            }),
+            React.createElement(Pet, {
+                name: "Pepper",
+                animal: "bird",
+                breed: "Cockatiel"
+            }),
+            React.createElement(Pet, {
+                name: "Doink",
+                animal: "cat",
+                breed: "Mixed"
+            })
+        ])
+
+    }
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
