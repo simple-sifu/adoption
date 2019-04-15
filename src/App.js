b5e2371521d23da62df0955902ca464d0a5c25fa
Tommy Han <tommy.han@lnhpmacj19djgh8.dt.inc>
2019-04-15 16:07:49 -0400

    add reusable component PET

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Luna"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h2", {}, "Havanese")
+    ]);
+}
 const App = () => {
-    return React.createElement("div", {}, 
-        React.createElement(('h1'), {}, 'Adopt Me!')
-    )
+    return React.createElement("div", {}, [
+        React.createElement(('h1'), {}, 'Adopt Me!'),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet)
+    ])
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
