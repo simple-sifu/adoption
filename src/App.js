885aa5be8048b2a49772725a98ed27fa3ceb9906
tommy.han <tommy.han@coxautoinc.com>
2019-04-16 23:08:12 -0400

    adding PetFinder Client with mock data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import pf from "petfinder-client";
+
+// under imports
+// Note (April 2nd, 2019): You no longer need real API keys, Petfinder Client will still work
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     pets: []
+  //   };
+
+  // }
+
+  componentDidMount() {
+    const promise = petfinder.breed.list({ animal: "dog" });
+    promise.then(console.log, console.error);
+  }
+
   handleTitleClick() {
     alert("you clicked the title");
   }
+
   render() {
     return (
       <div>
