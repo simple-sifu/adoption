e4c0f91161a5e907da9ee49095de45e1b83ab49d
tommy.han <tommy.han@coxautoinc.com>
2019-04-17 15:14:27 -0400

    create 2 new components

--- src/App.js
+++ src/App.js
@@ -1,72 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
-import pf from "petfinder-client";
-
-// under imports
-// Note (April 2nd, 2019): You no longer need real API keys, Petfinder Client will still work
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import { Router } from "@reach/router";
+import Results from "./Results";
+import Details from "./Details";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pets: []
-    };
-  }
-
-  componentDidMount() {
-    petfinder.pet
-      .find({ output: "full", location: "Seattle, WA" })
-      .then(data => {
-        let pets;
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet;
-          } else {
-            pets = [data.petfinder.pets.pet];
-          }
-        } else {
-          pets = [];
-        }
-        this.setState({
-          pets
-        });
-      });
-  }
-
-  handleTitleClick() {
-    alert("you clicked the title");
-  }
-
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <div>
-          {this.state.pets.map(pet => {
-            let breed;
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(", ");
-            } else {
-              breed = pet.breeds.breed;
-            }
-
-            return (
-              <Pet
-                key={pet.id}
-                animal={pet.animal}
-                name={pet.name}
-                breed={breed}
-                media={pet.media}
-                location={`${pet.contact.city}, ${pet.contact.state}`}
-              />
-            );
-          })}
-        </div>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
