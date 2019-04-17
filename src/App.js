4245077fcd70dbc0b0cc9f77812d0e3a01c04530
tommy.han <tommy.han@coxautoinc.com>
2019-04-17 14:12:47 -0400

    setup Pet component with all information

--- src/App.js
+++ src/App.js
@@ -54,12 +54,15 @@ class App extends React.Component {
             } else {
               breed = pet.breeds.breed;
             }
+
             return (
               <Pet
                 key={pet.id}
                 animal={pet.animal}
                 name={pet.name}
                 breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
               />
             );
           })}
